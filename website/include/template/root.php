c5e364493a8f6dff1d3a844eb2e27a7bfcef8387
Harri Perälä <harri.perala@iki.fi>
2018-11-06 12:53:01 +0200

    Improve mobile rendering

--- website/include/template/root.php
+++ website/include/template/root.php
@@ -5,6 +5,7 @@
 <?php echo $this->param('title'); ?>
 </title>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/text.css">
 <link rel="stylesheet" href="css/layout.css">
 <link rel="stylesheet" href="css/elements.css">
